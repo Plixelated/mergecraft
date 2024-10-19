03c3f86ccfbb8ec82d8c6600f7ea848acafa82ca
Plixelated <76276169+Plixelated@users.noreply.github.com>
2024-10-19 12:23:16 -0700

    Merge Table Added
    Added A custom merge table to the potion scriptable object

--- Assets/Editor/ArrayViewer2D.cs
+++ Assets/Editor/ArrayViewer2D.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class ArrayViewer2D : MonoBehaviour
+[CustomPropertyDrawer(typeof(MergeTable))]
+public class ArrayViewer2D : PropertyDrawer
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        
+        EditorGUI.PrefixLabel(position, label);
+
+        Rect newPosition = position;
+        newPosition.y += 18f;
+        SerializedProperty rows = property.FindPropertyRelative("rows");
+
+        for (int i = 0; i < 10; i++)
+        {
+            SerializedProperty row = rows.GetArrayElementAtIndex(i).FindPropertyRelative("row");
+            newPosition.height = 20;
+
+            if (row.arraySize != 10)
+                row.arraySize = 10;
+
+            newPosition.width = 70;
+
+            for (int j = 0; j < 10; j++)
+            {
+                EditorGUI.PropertyField(newPosition, row.GetArrayElementAtIndex(j), GUIContent.none);
+                newPosition.x += newPosition.width;
+            }
+
+            newPosition.x = position.x;
+            newPosition.y += 20;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        
+        return 20 * 12;
     }
 }
--- Assets/Scripts/Items/Potion.cs
+++ Assets/Scripts/Items/Potion.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName ="New Potion",menuName ="Potion")]
+
+[System.Serializable]
+public class Outputs
+{
+    public Potion output;
+}
+[System.Serializable]
+public class Merges
+{
+    public List<Potion> validMerge;
+    public Outputs mergeResult;
+}
+[System.Serializable]
+public class MergeList
+{
+    public List<Merges> mergeList;
+}
 public class Potion : ScriptableObject
 {
     public string potionName;
     public string description;
     public float value;
     public Sprite icon;
+
+    public MergeList mergeTable = new MergeList();
 
 }
